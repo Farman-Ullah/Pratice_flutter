d0306660e26814f565c339df1a22de77478762d1
Tech <ufarman352@gmail.com>
2024-07-13 21:45:23 -0700

    day-5 completed

--- lib/Pages/LoginPage.dart
+++ lib/Pages/LoginPage.dart
@@ -1,13 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_first_vs/Utils/Routes.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Center(
-        child: Text("Welcome to login page"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login Page"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+                width: 150,
+                height: 120,
+                child: Image.asset('assets/images/login.png')),
+            Text(
+              "Welcome",
+              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter Username",
+                      label: Text("Username"),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter Password",
+                      label: Text("Password"),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        //Navigator.pushNamed(context, MyRoutes.homeRoute);
+                      },
+                      child: Text("Login")),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_first_vs/Pages/HomePage.dart';
+//import 'package:flutter_first_vs/Pages/HomePage.dart';
 import 'package:flutter_first_vs/Pages/LoginPage.dart';
+//import 'package:flutter_first_vs/Utils/Routes.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,11 +14,11 @@ class MyApp extends StatelessWidget {
       themeMode: ThemeMode.dark,
       darkTheme: ThemeData(brightness: Brightness.light),
       theme: ThemeData(primarySwatch: Colors.amber),
-      initialRoute: "/home",
-      routes: {
-        "/": (context) => HomePage(),
-        "/home": (context) => HomePage(),
-        //"/login": (context) => LoginPage(),
+      //initialRoute: "/login",
+      home: const LoginPage(),
+      //routes: {
+        //MyRoutes.homeRoute: (context) => const HomePage(),
+        //MyRoutes.loginRoute: (context) => const LoginPage(),
       },
     );
   }
